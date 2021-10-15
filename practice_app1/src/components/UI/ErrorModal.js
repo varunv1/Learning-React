e34f068474399edf76484363b6dabd4cf709f216
Varun <varunv1a2r3@gmail.com>
2021-10-15 22:43:41 +0530

    added portals

--- practice_app1/src/components/UI/ErrorModal.js
+++ practice_app1/src/components/UI/ErrorModal.js
@@ -1,27 +1,46 @@
- import React from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+
 import Button from "./Button";
 import Card from "./Card";
-import styles from './ErrorModal.module.css';
- const ErrorModal = props => {
-    return(
-        <div onClick = {props.onConfirm}>
-            <div className = {styles.backdrop}>
+import styles from "./ErrorModal.module.css";
 
-            </div>
-        
-        <Card className = {styles.modal}>
-            <header className = {styles.header}>
-                <h2>{props.title}</h2>
-            </header>
-            <div className = {styles.content}>
-                <p>{props.message}</p>
-            </div>
-            <footer className = {styles.actions}>
-                <Button onClick = {props.onConfirm}>Okay</Button>
-            </footer>
-        </Card>
-        </div>
-    );
- };
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
 
- export default ErrorModal;
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+const ErrorModal = (props) => {
+  return(
+  <React.Fragment>
+    {ReactDOM.createPortal(
+      <Backdrop onConfirm={props.onConfirm} />,
+      document.getElementById("backdrop-root")
+    )}
+    {ReactDOM.createPortal(
+      <ModalOverlay
+      title={props.title}
+      message={props.message}
+      onConfirm={props.onConfirm}
+      />,
+      document.getElementById("overlay-root")
+    )}
+  </React.Fragment>
+
+  );
+    };
+export default ErrorModal;
