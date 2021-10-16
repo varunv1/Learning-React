8259d8d0ca87bb12c6f2c9d53f222656d96bb12b
Varun <varunv1a2r3@gmail.com>
2021-10-16 21:18:05 +0530

    added effect debouncing and cleanup fn

--- side_effect_handling/src/App.js
+++ side_effect_handling/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,14 +6,24 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(()=>{
+    const localStorageLoggedInInformation = localStorage.getItem('isLoggedIn');
+    if (localStorageLoggedInInformation === '1'){
+      setIsLoggedIn(true);
+    }
+  }, []);
+  // here this will just once because we have no dependency, so as the module gets rendered for
+  // the first time, it also gets rendered after all the components get rendered.
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', '1');
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
